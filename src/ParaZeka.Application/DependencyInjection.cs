c527e9a19b1e3ddb8c24d388d8c38c4c74cfc657
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-04-04 23:20:56 +0300

    Application Layer - CQRS and Imterfaces

---
+++ src/ParaZeka.Application/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaZeka.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(cfg => {
+                cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+                cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            });
+
+            return services;
+        }
+    }
+}
+
