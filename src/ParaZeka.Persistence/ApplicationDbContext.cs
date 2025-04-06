b4e2a1102ba0f156a15443fe89cda668f82faf88
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-04-06 23:27:46 +0300

    ApplicationDbContext Added

---
+++ src/ParaZeka.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore;
+using ParaZeka.Application.Common.Interfaces;
+using ParaZeka.Domain.Common;
+using ParaZeka.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParaZeka.Persistence
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        private readonly ICurrentUserService _currentUserService;
+
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options,
+            ICurrentUserService currentUserService) : base(options)
+        {
+            _currentUserService = currentUserService;
+        }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Account> Accounts => Set<Account>();
+        public DbSet<Transaction> Transactions => Set<Transaction>();
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Budget> Budgets => Set<Budget>();
+        public DbSet<FinancialGoal> FinancialGoals => Set<FinancialGoal>();
+        public DbSet<FinancialInsight> FinancialInsights => Set<FinancialInsight>();
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (EntityEntry<BaseEntity> entry in ChangeTracker.Entries<BaseEntity>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.UtcNow;
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.UtcNow;
+                        break;
+                }
+            }
+
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
