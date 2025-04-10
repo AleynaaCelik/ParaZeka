89c2f7d03f8289fd8ae7d0ff2caa34741a36d886
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-04-10 21:54:40 +0300

    Error handling Middleware

---
+++ src/ParaZeka.API/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace ParaZeka.API.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ErrorHandlingMiddleware> _logger;
+
+        public ErrorHandlingMiddleware(
+            RequestDelegate next,
+            ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            _logger.LogError(exception, "An error occurred: {ErrorMessage}", exception.Message);
+
+            var code = HttpStatusCode.InternalServerError; // 500 if unexpected
+            var result = JsonSerializer.Serialize(new { error = exception.Message });
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
