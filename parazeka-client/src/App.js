ef7adca4cc55cc54d5f5b3d7b67b00e278e614d3
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-04-17 16:06:11 +0300

    Pages was Created

--- parazeka-client/src/App.js
+++ parazeka-client/src/App.js
@@ -1,9 +1,11 @@
-// src/App.js (güncellenen)
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/common/Navbar';
 import Dashboard from './pages/Dashboard';
 import Transactions from './pages/Transactions';
+import Accounts from './pages/Accounts';
+import Budgets from './pages/Budgets';
+import Goals from './pages/Goals';
 import './App.css';
 
 function App() {
@@ -15,7 +17,9 @@ function App() {
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/transactions" element={<Transactions />} />
-            {/* Diğer sayfalar için route'lar buraya eklenecek */}
+            <Route path="/accounts" element={<Accounts />} />
+            <Route path="/budgets" element={<Budgets />} />
+            <Route path="/goals" element={<Goals />} />
           </Routes>
         </main>
       </div>
