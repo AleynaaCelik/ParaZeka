e094fe9341737557886422297ac733c35e476ce4
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2025-04-18 15:07:21 +0300

    Creating API Services

---
+++ parazeka-client/src/api/axiosConfig.js
@@ -0,0 +1,25 @@
+// src/api/axiosConfig.js
+import axios from 'axios';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:5211/api', // Backend URL'iniz
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// Request interceptor
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
